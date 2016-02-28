c438d7179baf4beec104ea0a599421a11a25a8d3
gf2595 <Ehre47593Treue>
2016-02-29 02:39:34 +0300

    Homework 19.02 initial commit

---
+++ 19.02/19.02/Program.fs
@@ -0,0 +1,30 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+
+let rec factorial x = 
+    if x = 1 then 1 else x * factorial (x - 1)
+
+let rec fibonacci x = 
+    if x = 1 then 1 else if x = 2 then 1 else fibonacci (x - 1) + fibonacci (x - 2)
+
+let reverseList list =
+    let length = List.length list
+    List.init length (fun i -> List.nth list (length - i - 1))
+
+let buildList length = 
+    let rec binaryPower i = 
+        if i = 0 then 1 else 2 * binaryPower (i - 1)
+    List.init length (fun i -> binaryPower i)
+
+[<EntryPoint>]
+let main argv = 
+    let x = factorial (3)
+    printfn "%d" x
+
+    let list = buildList 10
+
+    let reversedlist = reverseList list
+
+    printfn "%A" argv
+    0 // возвращение целочисленного кода выхода
